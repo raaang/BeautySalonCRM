ec849629c964a2bc28285ab6480be2e93b80aef6
cloud0406 <cloud0406@naver.com>
2023-05-18 21:13:25 +0900

    [MU] Feat: 회원 상세 정보 (회원 정보) - #34
    * 회원 상세정보 - 회원 정보
    * config 데이터 추가

---
+++ beautysalon-frontend/src/components/member/moreInfo/memberInfoArticle.tsx
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import React from 'react';
+import TextRow from '../../../components/common/textRow';
+import ArticleHeader from '../../../components/layout/articleHeader';
+import { MemberInfoConfig } from '../../../config/moreInfoConfig';
+
+function MemberInfoArticle() {
+  return (
+    <article
+      css={css`
+        width: 100%;
+        margin-bottom: 100px;
+      `}
+    >
+      <ArticleHeader title="회원 정보" btnName="정보 수정"></ArticleHeader>
+      <div>
+        {MemberInfoConfig.map((item, idx) => {
+          if (item.type === 'text') {
+            return <TextRow key={idx} title={item.title} content={item.placeholder}></TextRow>;
+          }
+        })}
+      </div>
+    </article>
+  );
+}
+
+export default MemberInfoArticle;
